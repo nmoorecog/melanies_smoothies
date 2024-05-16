cd2a198b3236d322691ccaec7a0b7bc108249f62
nmoorecog <144072113+nmoorecog@users.noreply.github.com>
2024-05-16 14:45:42 +1200

    add fruit chosen for nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,27 +3,21 @@ import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
-# Write directly to the app
+# Introduction for the app
 st.title("Customize Your Smoothie :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom Smoothie!
-    """
-)
+st.write("""Choose the fruits you want in your custom Smoothie!""")
 
+# name for the order
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be: ', name_on_order)
 
+# connection information to get to Snowflake
 cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
-
+# max ingredients is weird but fine
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe, 
@@ -36,6 +30,9 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     # st.write(ingredients_string)
 
