75e9781605615659c0a93e3f25384c8704a21fd9
nmoorecog <144072113+nmoorecog@users.noreply.github.com>
2024-05-16 15:00:07 +1200

    convert to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,11 @@ st.write('The name on your smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 # max ingredients is weird but fine
 ingredients_list = st.multiselect(
